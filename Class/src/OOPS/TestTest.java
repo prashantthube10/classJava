39e15299d1add81c3ea395fc938db9afd1533107
Prashant Thube <prashantthube10@gmail.com>
2019-11-03 11:47:39 +0530

    saf

--- Class/src/OOPS/TestTest.java
+++ Class/src/OOPS/TestTest.java
@@ -4,12 +4,12 @@ public class TestTest {
 
 	public static void main(String[] args) {
 
-		//from git// here only chnage is there from the git////conflict
+		// from eclipse// here only chnage is there from the git////conflict
 //git
 //// here is my chnages from the esclipsec
 		// this is my new chnages at the kine 8
 //git
-		//git
+		// git
 	}
 
 }
