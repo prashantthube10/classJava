9010c9edcaee605b8148a9a38a90fa3d760469ce
prashantthube10 <51267303+prashantthube10@users.noreply.github.com>
2019-11-03 18:38:07 +0530

    Update TestTest.java

--- Class/src/OOPS/TestTest.java
+++ Class/src/OOPS/TestTest.java
@@ -1,27 +1,7 @@
 package OOPS;
 
 public class TestTest {
-	// idid some chnages
-
-	public static void main(String[] args) {
-
-		// from git// here only chnage is there from the git////conflict
-//git
-//// here is my chnages from the esclipsec
-		// this is my new chnages at the kine 8
-//git
-		// git
-		// i'm writing at the same line
-
-		// want to merge
-		// hhahhhave some n jnjndfjsjsjdnjfn// pplease here check that once u statsh it
-		// there is a cnages is coming directly
-		// not checking after statsjasdfasdadadasdadasdasdasd
-//<<<<<<< HEAD
-		// 20 ecplise
-//=======
-		// 200000000000000 git
-//>>>>>>> branch 'master' of https://github.com/prashantthube10/classJava.git
+	
 	}
 
 }
