c315e5c2084142725e6d5915082f57c1a36913ac
prashantthube10 <51267303+prashantthube10@users.noreply.github.com>
2019-11-03 11:49:20 +0530

    Update TestTest.java

--- Class/src/OOPS/TestTest.java
+++ Class/src/OOPS/TestTest.java
@@ -4,7 +4,7 @@ public class TestTest {
 
 	public static void main(String[] args) {
 
-		// from eclipse// here only chnage is there from the git////conflict
+		// from git// here only chnage is there from the git////conflict
 //git
 //// here is my chnages from the esclipsec
 		// this is my new chnages at the kine 8
