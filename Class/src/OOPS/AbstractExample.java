921f61eeb250382c5d7d7ce70634e25c4b032d6f
PrashantThube <prashantthube10@gmail.com>
2020-06-29 00:35:14 +0530

    commtting the abstract class after conflict resolved after doing some change

--- Class/src/OOPS/AbstractExample.java
+++ Class/src/OOPS/AbstractExample.java
@@ -7,6 +7,8 @@ public class AbstractExample extends Abstraction {
 
 		// TODO Auto-generated method stub on the same line i have done some chnages from website line number 7
 		return null;
+	
+		//your new chnages are here from local at line number 11.
 	}
 
 	
